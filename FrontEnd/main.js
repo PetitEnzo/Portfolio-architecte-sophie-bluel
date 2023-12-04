aacdd8e4dd367e648e2aa7d9a1dca2cc497f8f42
Enzo Petit <enzopetit33800@gmail.com>
2023-12-04 21:48:37 +0100

    fix du login et de la page admin

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,39 +1,68 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const apiUrl = "http://localhost:5678/api/works";
+const apiUrl = "http://localhost:5678/api";
+const apiEnd = (endpoint) => apiUrl + endpoint;
+console.log(apiEnd("/works"));
 
-  fetch(apiUrl)
+function userLog() {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    const mainHeader = document.getElementById("placementHeaderAdmin");
+    const editionBlock = document.createElement("div");
+    editionBlock.id = "edition";
+    editionBlock.innerHTML = `
+    <i class="fa-regular fa-pen-to-square"></i>
+    <p>mode d'édition</p>
+  `;
+    mainHeader.appendChild(editionBlock);
+
+    const logoutLink = document.getElementById("loginLink");
+    logoutLink.textContent = "logout";
+    logoutLink.addEventListener("click", function () {
+      localStorage.removeItem("token");
+      window.location.href = "index.html";
+    });
+    const filterButtons = document.getElementById("filter-buttons");
+    filterButtons.remove();
+
+    const ProjetAdmin = document.getElementById("Project");
+    const AdminBlock = document.createElement("div");
+    AdminBlock.id = "Projet-Admin";
+    AdminBlock.innerHTML = `
+    <i class="fa-regular fa-pen-to-square"></i>
+    <p>modifier</p>
+    `;
+    ProjetAdmin.appendChild(AdminBlock);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetch(apiEnd("/works"))
     .then((response) => response.json())
     .then((data) => {
+      userLog();
       const galerie = document.getElementById("portfolio");
 
-      // Vérifie l'élement avant de modifier le contenu
       if (galerie) {
-        // Supprimez mes anciennes photos et titre
         galerie.innerHTML = "";
 
-        // Ajoute chaque projet à la galerie
         data.forEach((projet) => {
           const nouvelElement = document.createElement("div");
 
-          //ajout de class sur mes div, des categories des projets
           nouvelElement.classList.add("modal-div");
           nouvelElement.setAttribute("data-category", projet.category.name);
           nouvelElement.innerHTML = `
-          <img src="${projet.imageUrl}" alt="${projet.title}" class="modal-image">
-          <h3>${projet.title}</h3>
-        `;
+            <img src="${projet.imageUrl}" alt="${projet.title}" class="modal-image">
+            <h3>${projet.title}</h3>
+          `;
           galerie.appendChild(nouvelElement);
         });
-      } else {
-        console.error("L'élément avec l'ID 'portfolio' n'a pas été trouvé.");
       }
     })
     .catch((error) =>
       console.error("Erreur lors de la récupération des données :", error)
     );
 
-  //ajout de la fonction filtre
-
+  // Fonction de filtre des projets
   function filterProjects(category) {
     const projects = document.querySelectorAll(".modal-div");
 
@@ -48,37 +77,78 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  // Ajoutez un écouteur d'événements à chaque bouton
+  // Fonction d'annulation de l'affichage des autres boutons en activ
+  const buttonremove = () => {
+    document
+      .querySelectorAll("#filter-buttons button")
+      .forEach((button) => button.classList.remove("active"));
+  };
+
+  const clickbutton = (button, filterType) => {
+    buttonremove();
+    filterProjects(filterType);
+    button.classList.add("active");
+  };
+
+  // Ajout d'un écouteur d'événements à chaque bouton de filtre
   document.getElementById("allBtn").addEventListener("click", function () {
-    filterProjects("all");
-    this.classList.toggle("active");
+    clickbutton(this, "all");
   });
 
   document.getElementById("objetsBtn").addEventListener("click", function () {
-    filterProjects("Objets");
-    this.classList.toggle("active");
+    clickbutton(this, "Objets");
   });
 
   document
     .getElementById("appartementsBtn")
     .addEventListener("click", function () {
-      filterProjects("Appartements");
-      this.classList.toggle("active");
+      clickbutton(this, "Appartements");
     });
 
   document
     .getElementById("hotelsRestaurantsBtn")
     .addEventListener("click", function () {
-      filterProjects("Hotels & restaurants");
-      this.classList.toggle("active");
+      clickbutton(this, "Hotels & restaurants");
     });
+});
 
-  // Récupérer le lien de connexion
-  const loginLink = document.getElementById("loginLink");
+// Récupération du lien de connexion
+const loginLink = document.getElementById("loginLink");
 
-  // Ajouter un écouteur d'événements au clic
-  loginLink.addEventListener("click", function () {
-    // Ajoutez ici le code pour gérer la connexion
-    window.location.href = "login.html"; // Remplacez ceci par votre logique de connexion
-  });
+loginLink.addEventListener("click", function () {
+  window.location.href = "login.html";
+});
+
+const loginForm = document.getElementById("loginForm");
+// Ajout d'un écouteur d'événements à la soumission du formulaire
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  // Récupération des valeurs du formulaire
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  // Envoi de la requête API de connexion
+  fetch(apiEnd("/users/login"), {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: username,
+      password,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.token) {
+        localStorage.setItem("token", data.token); // Stockage local du token
+        window.location.href = "index.html";
+        userLog();
+      } else {
+        const errorMessage = document.createElement("p");
+        errorMessage.textContent = "Identifiants incorrects.";
+        loginForm.appendChild(errorMessage);
+      }
+    });
 });
