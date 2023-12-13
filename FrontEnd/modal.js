0acd7599d8e737dde1cc117555f589ffd6902482
Enzo Petit <enzopetit33800@gmail.com>
2023-12-13 17:19:06 +0100

    test vidage modal

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -7,6 +7,7 @@ const modalContainer = document.querySelector(".modal-container");
 const modalTriggers = document.querySelectorAll(".modal-trigger");
 const addPictureButton = document.querySelector(".AddPicture");
 const modalAjout = document.querySelector(".modal-ajout");
+const modal = document.querySelector(".modal");
 const closeModalButton = document.querySelector(".close-modal");
 const ArrowBack = document.querySelector(".fa-arrow-left");
 
@@ -16,6 +17,7 @@ addPictureButton.addEventListener("click", () => {
 
 ArrowBack.addEventListener("click", () => {
   modalAjout.classList.remove("active");
+  resetModalAjout();
 });
 
 modalTriggers.forEach((trigger) =>
@@ -25,9 +27,13 @@ modalTriggers.forEach((trigger) =>
 function toggleModal() {
   modalContainer.classList.toggle("active");
   modalAjout.classList.remove("active");
+  modal.classList.remove("active");
+  resetModalAjout();
 }
-
-closeModalButton.addEventListener("click", toggleModal);
+closeModalButton.addEventListener("click", () => {
+  toggleModal();
+  resetModalAjout(); // Ajout de cette ligne si nécessaire
+});
 
 fetch(apiEnd("/works"))
   .then((response) => response.json())
@@ -63,6 +69,7 @@ fetch(apiEnd("/works"))
     });
   });
 
+// Ajout de photo
 document.addEventListener("DOMContentLoaded", () => {
   const btnPicture = document.getElementById("btnPicture");
   const titleInput = document.getElementById("title");
@@ -130,8 +137,13 @@ document.addEventListener("DOMContentLoaded", () => {
         .then((response) => {
           const ProjectUpload = newProject(response);
           document.getElementById("portfolio").appendChild(ProjectUpload);
-          // Mise à jour de l'apparence du bouton après l'envoi de la photo
+          refreshGallery();
+
+          // Mise à jour de la modal après l'envoi de la photo (bouton, fermeture, vide de la div "inputPicture")
+          const previewContainer = document.querySelector(".inputPicture");
+
           updateSubmitButtonAppearance();
+          closeModalAfterSubmit();
         })
         .catch((error) => {
           console.error("Error during POST request:", error);
@@ -153,3 +165,104 @@ document.addEventListener("DOMContentLoaded", () => {
   // Appel initial pour désactiver le bouton
   updateSubmitButtonAppearance();
 });
+
+// cette fonction marche, elle me permets d'avoir un affichage dynamique de la gallerie, mais ne fait elle pas doublon ? Refacto possible ?
+function refreshGallery() {
+  fetch(apiEnd("/works"))
+    .then((response) => response.json())
+    .then((data) => {
+      const galerieModale = document.getElementById("portfolioModal");
+      galerieModale.innerHTML = "";
+
+      data.forEach((projet) => {
+        const imageContainer = document.createElement("div");
+        imageContainer.classList.add("imageContainer");
+        imageContainer.setAttribute("id", projet.id);
+
+        // Crée l'élément image
+        const imageModale = document.createElement("img");
+        imageModale.classList.add("imageModale");
+        imageModale.src = projet.imageUrl;
+
+        // Ajoute l'image à la div
+        imageContainer.appendChild(imageModale);
+
+        // Crée l'icône poubelle
+        const poubelle = document.createElement("i");
+        poubelle.classList.add("fa-solid", "fa-trash-can", "trash-icon");
+        poubelle.addEventListener("click", () => {
+          const token = localStorage.getItem("token");
+          const imageId = document.getElementById(`${projet.id}`).id;
+          deleteImage(imageId, token, imageModale);
+        });
+
+        // Ajoute l'icône à la div du conteneur
+        imageContainer.appendChild(poubelle);
+
+        // Ajoute la div du conteneur à la galerie modale
+        galerieModale.appendChild(imageContainer);
+      });
+    });
+}
+
+function closeModalAfterSubmit() {
+  modalContainer.classList.remove("active");
+  modalAjout.classList.remove("active");
+  modal.classList.remove("active");
+  resetModalAjout();
+}
+
+function resetModalAjout() {
+  // Réinitialise la div 'inputPicture'
+  const inputPictureDiv = document.querySelector(".inputPicture"); // a partir d'ici
+  inputPictureDiv.innerHTML = `
+  <i class="fa-regular fa-image"></i>
+  <label class="uploadPicture">
+      <input
+          type="file"
+          accept=".jpg,.png"
+          max-size="4000"
+          id="btnPicture"
+          class="btnPicture"
+          name="imageUrl"
+      />+ Ajouter photo
+  </label>
+  <br />
+  <p>Jpg, png : 4mo max</p>`;
+  btnPicture.addEventListener("change", (e) => {
+    const previewContainer = document.querySelector(".inputPicture");
+    const existingPreviewImage =
+      previewContainer.querySelector(".preview-image");
+
+    if (existingPreviewImage) {
+      existingPreviewImage.remove();
+    }
+
+    const file = e.target.files[0];
+
+    if (file) {
+      const previewImage = document.createElement("img");
+      previewImage.classList.add("preview-image");
+
+      previewImage.addEventListener("load", () => {
+        previewContainer.innerHTML = "";
+        previewContainer.appendChild(previewImage);
+        updateSubmitButtonAppearance();
+      });
+
+      previewImage.src = URL.createObjectURL(file);
+    }
+  }); // jusqu'a la, ca marche pas
+
+  // Réinitialise le champ de texte pour le titre
+  const titleInput = document.getElementById("title");
+  if (titleInput) {
+    titleInput.value = "";
+  }
+
+  // Réinitialise le menu déroulant pour la catégorie
+  const categorySelect = document.getElementById("CategorieId");
+  if (categorySelect) {
+    categorySelect.selectedIndex = 0; // Réinitialise à la première option
+  }
+}
